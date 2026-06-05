8de0dd584d56400cb9b53383fc063bd5bf05825f
kalyanashis <kalyanashis.13@gmail.com>
2026-06-06 00:17:53 +0530

    Add database schema script and remove unused imports from AESEncryptionUtil.java file

--- common-security/src/main/java/payment/app/common_security/util/AESEncryptionUtil.java
+++ common-security/src/main/java/payment/app/common_security/util/AESEncryptionUtil.java
@@ -1,7 +1,5 @@
 package payment.app.common_security.util;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import payment.app.common_security.exception.EncryptionException;
 
 import javax.crypto.Cipher;
